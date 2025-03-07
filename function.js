28e052fb4fe3fb2fe7ff9396cb1df29fcb783116
Kshitij Nayan <kshitijnayan9a28@gmail.com>
2025-03-07 22:33:08 +0530

    added extra features
    left: adding functions for x^y and applying brackets at end and start while using x^2 and x^y feature

--- function.js
+++ function.js
@@ -1,11 +1,27 @@
 let newTrue = false;
+let brac_count=0;
         
         function Display_d(value) {
+            let display=document.getElementById('display')
+            expression=display.value;
+            
             if (newTrue) {
                 Clear();
                 newTrue = false;
             }
-            document.getElementById('display').value += value;
+            if(value==')')
+            {
+                brac_count--;
+            }
+            if(value=='(')
+            {
+                brac_count++;
+            }
+            if(value.endsWith('sin')||value.endsWith('cos')||value.endsWith('tan'))
+            {
+                brac_count++;
+            }
+            document.getElementById('display').value+= value;
         }
         
         function Clear() {
@@ -15,10 +31,20 @@ let newTrue = false;
         
         function Solve() {
             try {
-                document.getElementById('display').value = eval(document.getElementById('display').value);
+                let display=document.getElementById('display')
+                expression=display.value;
+                while(brac_count)
+                {
+                    expression+=')';
+                    brac_count--;
+                }    
+                expression=expression.replace(/sin\(([^)]+)\)/g,(match,angle) => Math.sin(parseFloat(angle)*(3.14/180)));
+                expression=expression.replace(/cos\(([^)]+)\)/g,(match,angle) => Math.cos(parseFloat(angle)*(3.14/180)));
+                expression=expression.replace(/tan\(([^)]+)\)/g,(match,angle) => Math.tan(parseFloat(angle)*(3.14/180)));
+                document.getElementById('display').value = eval(expression);
                 newTrue = true;
             } catch {
                 document.getElementById('display').value = 'Error';
-                newTrue = false;
+                newTrue = true;
             }
         }
